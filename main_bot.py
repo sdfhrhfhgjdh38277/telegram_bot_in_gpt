d6d489ec556279a02f5a5713f017f68f7683e243
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-02-18 20:50:44 +0200

    Add files via upload

---
+++ main_bot.py
@@ -0,0 +1,31 @@
+import g4f
+import telebot as tele
+import token
+bot = tele.TeleBot(token)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(
+        message,
+        "Привет, я чат-бот основанный на ChatGPT 3.5. Ты можешь задать мне любой вопрос! Я отвечу на него."
+        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!",
+    )
+
+
+@bot.message_handler(content_types=["text"])
+def text(message):
+    response = (
+        (
+            g4f.ChatCompletion.create(
+                model=g4f.models.gpt_35_turbo,
+                messages=[{"role": "user", "content": message}],
+                language="ru",
+            ),
+        ),
+    )
+    bot.send_message(message.chat.id, response)
+    print(message)
+
+
+bot.infinity_polling()
