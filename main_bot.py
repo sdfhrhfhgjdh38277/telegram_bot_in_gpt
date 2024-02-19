ae39a471ea96dca4354ef961488920738de64150
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-02-19 21:25:20 +0200

    Update main_bot.py
    Заменил модель генерации текста, также добавил "оповещение" про обновление бота

--- main_bot.py
+++ main_bot.py
@@ -1,27 +1,30 @@
 import g4f
 import telebot as tele
-import token
-bot = tele.TeleBot(token)
+
+bot = tele.TeleBot("6719622488:AAEvOetK4PPPJs8a8LMqZAke5isOHFLMzpg")
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
+
     bot.reply_to(
         message,
         "Привет, я чат-бот основанный на ChatGPT 3.5. Ты можешь задать мне любой вопрос! Я отвечу на него."
-        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!\nОт разработчика - напиши язык на котором хочешь сделать вывод(Иначе выведется на английском)",
+        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!\nОт разработчика - напиши "
+        "язык на котором хочешь сделать вывод(Иначе выведется на английском)",
+    )
+    bot.send_message(
+        message.chat.id,
+        "Обновление 0.2 - Изменена система поиска: вместо обычных сообщений теперь выступает команда /search ("
+        "запрос). В дальнейшем планируется добавить поддержку групп",
     )
 
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(commands=["search"])
 def text(message):
-    response = (
-        (
-            g4f.ChatCompletion.create(
-                model=g4f.models.gpt_35_turbo,
-                messages=[{"role": "user", "content": message}],
-            ),
-        ),
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": message.text}],
     )
     bot.send_message(message.chat.id, response)
 
