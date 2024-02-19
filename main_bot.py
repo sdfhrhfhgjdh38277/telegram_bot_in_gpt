3de294c22212fb49ee573fd7c2e6fb83fe60c555
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-02-19 14:47:23 +0200

    Update main_bot.py
    Изменено пару строк кода, некоторые убраны

--- main_bot.py
+++ main_bot.py
@@ -9,7 +9,7 @@ def start(message):
     bot.reply_to(
         message,
         "Привет, я чат-бот основанный на ChatGPT 3.5. Ты можешь задать мне любой вопрос! Я отвечу на него."
-        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!",
+        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!\nОт разработчика - напиши язык на котором хочешь сделать вывод(Иначе выведется на английском)",
     )
 
 
@@ -20,12 +20,10 @@ def text(message):
             g4f.ChatCompletion.create(
                 model=g4f.models.gpt_35_turbo,
                 messages=[{"role": "user", "content": message}],
-                language="ru",
             ),
         ),
     )
     bot.send_message(message.chat.id, response)
-    print(message)
 
 
 bot.infinity_polling()
