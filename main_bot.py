b22980b05b9bc9ca2bdb5f223544ec5fc89aba95
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-02-20 14:43:23 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,32 +1,40 @@
 import g4f
 import telebot as tele
+from time import time
 
-bot = tele.TeleBot("You_token")
+bot = tele.TeleBot("6719622488:AAEvOetK4PPPJs8a8LMqZAke5isOHFLMzpg")
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-
+    # hello message, and update notification
     bot.reply_to(
         message,
-        "Привет, я чат-бот основанный на ChatGPT 4. Ты можешь задать мне любой вопрос! Я отвечу на него."
+        "Привет, я чат-бот основанный на ChatGPT 3.5. Ты можешь задать мне любой вопрос! Я отвечу на него."
         "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!\nОт разработчика - напиши "
         "язык на котором хочешь сделать вывод(Иначе выведется на английском)",
     )
     bot.send_message(
         message.chat.id,
         "Обновление 0.2 - Изменена система поиска: вместо обычных сообщений теперь выступает команда /search ("
-        "запрос). В дальнейшем планируется добавить поддержку групп, также изменена система генерации - теперь вместо 3.5 которая могла быть устаревшей поставлена новая версия - 4. В работе могут быть долгие генерации, поэтому наберитесь терпения:)",
+        "запрос). В дальнейшем планируется добавить поддержку групп",
     )
 
 
 @bot.message_handler(commands=["search"])
-def text(message):
+def ask_gpt(message):
     response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_4,
-        messages=[{"role": "user", "content": message.text}],
+        model=g4f.models.gpt_4_32k,
+        messages=[
+            {"role": "user", "content": message.ask_gpt},
+            {
+                "role": "assistant",
+                "content": ...,
+            },  # there must be contained last bot message, but i don`t make this in soon.
+        ],
     )
     bot.send_message(message.chat.id, response)
+    print(f"Request generated in: {time()}")
 
 
 bot.infinity_polling()
