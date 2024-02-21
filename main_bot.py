8a49b308a3e13aa2fe6b8809d923e0cd36332951
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-02-21 14:54:23 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -2,7 +2,7 @@ import g4f
 import telebot as tele
 from time import time
 
-bot = tele.TeleBot("6719622488:AAEvOetK4PPPJs8a8LMqZAke5isOHFLMzpg")
+bot = tele.TeleBot("token")
 
 
 @bot.message_handler(commands=["start"])
@@ -10,28 +10,14 @@ def start(message):
     # hello message, and update notification
     bot.reply_to(
         message,
-        "Привет, я чат-бот основанный на ChatGPT 3.5. Ты можешь задать мне любой вопрос! Я отвечу на него."
-        "Но я могу не слишком точно давать ответы, поэтому прости если что-то будет не так!\nОт разработчика - напиши "
-        "язык на котором хочешь сделать вывод(Иначе выведется на английском)",
-    )
-    bot.send_message(
-        message.chat.id,
-        "Обновление 0.2 - Изменена система поиска: вместо обычных сообщений теперь выступает команда /search ("
-        "запрос). В дальнейшем планируется добавить поддержку групп",
-    )
+        "Привет, я чат-бот основанный на ChatGPT 4. Ты можешь задать мне любой вопрос! Я отвечу на него."
+        )
 
-
-@bot.message_handler(commands=["search"])
+@bot.message_handler(content_types=["text"])
 def ask_gpt(message):
     response = g4f.ChatCompletion.create(
         model=g4f.models.gpt_4_32k,
-        messages=[
-            {"role": "user", "content": message.ask_gpt},
-            {
-                "role": "assistant",
-                "content": ...,
-            },  # there must be contained last bot message, but i don`t make this in soon.
-        ],
+        messages=[{"role": "user", "content": message}],
     )
     bot.send_message(message.chat.id, response)
     print(f"Request generated in: {time()}")
